8fad1071e1792f8a4598718e4c2a9951a813d1dc
Afish <2054641315@qq.com>
2025-06-23 16:43:59 +0800

    添加项目文件。

---
+++ MazeGenerator.h
@@ -0,0 +1,9 @@
+#pragma once
+//ÃÔ¹¬Éú³ÉÄ£¿é
+#include<iostream>
+using namespace std;
+class MazeGenerator {
+public:
+	void generate(int n);   
+
+};
---
+++ Utils.h
@@ -0,0 +1,4 @@
+#pragma once
+//公共工具函数模块
+#include<iostream>
+using namespace std;
